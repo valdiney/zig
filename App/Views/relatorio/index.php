c9cf7d6751eac1fc27d6c0ea0ebd5bf0bf44598d
valdiney <valdiney.2@hotmail.com>
2020-05-09 01:24:36 -0300

    Começando a fazer a tela de relatorios

--- App/Views/relatorio/index.php
+++ App/Views/relatorio/index.php
@@ -1,1 +1,51 @@
-<h1>Teste</h1>
+<style type="text/css">
+	.card-option {
+		background:#f7f7f7;
+		border:1px solid #51cbce;
+	}
+	.icon-sale {
+		font-size:50px;
+		display:block;
+		margin:0 auto;
+	}
+</style>
+
+<div class="row">
+
+	<div class="card col-lg-12 content-div">
+		<div class="card-body">
+	        <h5 class="card-title">Relatórios</h5>
+	    </div>
+
+      
+	  <div class="row">
+
+	  	<div class="col-lg-4">
+		   	 <div class="card card-option" style="width: 18rem;">
+				  <div class="card-body">
+				  	<center><i class="fas fa-piggy-bank icon-sale" style="color:#ff9b9b"></i></center> <br>
+				    <center><a href="#" class="btn btn-primary">Vendas por período</a></center>
+				 </div>
+			  </div>
+	     </div>
+
+	     <div class="col-lg-4">
+		   	 <div class="card card-option" style="width: 18rem;">
+				  <div class="card-body">
+				  	<center><i class="fas fa-piggy-bank icon-sale" style="color:#9be6e6"></i></center> <br>
+				    <center><a href="#" class="btn btn-primary">Comparação entre períodos</a></center>
+				 </div>
+			  </div>
+	     </div>
+
+	  
+	  	
+	  </div>
+
+		
+
+		
+    <br>
+	
+   </div>
+</div>

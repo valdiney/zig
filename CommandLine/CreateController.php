528f822ba893239ea5d17002eccddc422a9a966f
valdiney <valdiney.2@hotmail.com>
2020-05-16 03:03:48 -0300

    Criando ajudadores de linha de comando

---
+++ CommandLine/CreateController.php
@@ -0,0 +1,57 @@
+<?php
+
+function controller($nome) {
+	$arquivo = fopen("App/Controllers/{$nome}.php",'w');
+	$diretive = '$this->';
+	$vars = '$';
+
+$texto = "<?php
+namespace App\Controllers;
+use System\Controller\Controller;
+use System\Post\Post;
+use System\Get\Get;
+use System\Session\Session;
+
+class {$nome} 
+{
+	protected {$vars}post;
+	protected {$vars}get;
+	protected {$vars}layout;
+
+	public function __construct()
+	{
+		parent::__construct();
+		{$diretive}layout = 'default';
+
+		{$diretive}post = new Post();
+		{$diretive}get = new Get();
+	}
+
+	public function index()
+	{
+		{$diretive}view('login/index', {$diretive}layout);
+	}
+
+	public function save()
+	{
+		# Escreva aqui...
+	}
+
+	public function update()
+	{
+		# Escreva aqui...
+	}
+}
+
+";
+
+   fwrite($arquivo, $texto);
+   fclose($arquivo);
+
+	if ($arquivo) {
+		return true;
+	} else {
+		return false;
+	}
+}
+?>

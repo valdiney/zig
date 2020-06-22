1c31f06a1be3f46cd40bd233722677e427b619b5
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-06-22 10:18:46 -0300

    add verificação exibir erros somente no ENV local

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
-<?php 
+<?php
+
+require_once(__DIR__ . '/vendor/autoload.php');
+
+# Load env configuration
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
 header('Access-Control-Allow-Origin: *');
 header('Content-type: text/html; charset=UTF-8');
 
-ini_set('display_errors',1);
-ini_set('display_startup_erros',1);
-error_reporting(E_ALL);
+if (getenv('APP_ENV', 'local') != 'production' && getenv('APP_DISPLAY_ERRORS', 'false') == 'true') {
+  ini_set('display_errors',1);
+  ini_set('display_startup_erros',1);
+  error_reporting(E_ALL);
+}
 
 // 43200 sessão ativa por 12h
 ini_set('session.cookie_lifetime', 43200);
@@ -18,10 +27,6 @@ require_once(__DIR__ . '/vendor/autoload.php');
 
 use System\Route\GetRoute;
 use System\Route\SelectController;
-
-# Load env configuration
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
 
 # Load controllers
 $route = new SelectController(new GetRoute);

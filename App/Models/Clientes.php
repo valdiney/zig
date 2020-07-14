654136da1f12132a2cc5db405f60bd736d088c67
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-07-14 09:59:29 -0300

    configura model clientes com a nova conexao

--- App/Models/Clientes.php
+++ App/Models/Clientes.php
@@ -5,71 +5,72 @@ use System\Database\Model;
 
 class Clientes extends Model
 {
-    protected $timestamps = true;
+  protected $table = 'clientes';
+  protected $timestamps = true;
 
-    public function clientes($idEmpresa)
-    {
-      dd($this->select('id', 'nome')->all());
-      return;
-    	return $this->query("
-    		SELECT cl.id, cl.nome, cl.email, cl.cnpj, cl.cpf, cl.telefone, cl.celular, cl.deleted_at,
-			cs.id AS idSegmento, cs.descricao AS descricaoSegmento,
-			ct.id AS idClienteTipo, ct.descricao AS descricaoClienteTipo
-			FROM clientes AS cl
-			LEFT JOIN clientes_segmentos AS cs ON cl.id_cliente_segmento = cs.id
-			LEFT JOIN clientes_tipos AS ct ON cl.id_cliente_tipo = ct.id
-			WHERE cl.id_empresa = {$idEmpresa}");
+  public function clientes($idEmpresa)
+  {
+    return $this->as('cl')
+    ->select(
+      'cl.id', 'cl.nome', 'cl.email', 'cl.cnpj', 'cl.cpf', 'cl.telefone', 'cl.celular', 'cl.deleted_at',
+      'cs.id AS idSegmento', 'cs.descricao AS descricaoSegmento',
+      'ct.id AS idClienteTipo', 'ct.descricao AS descricaoClienteTipo'
+    )
+    ->join('LEFT JOIN clientes_segmentos AS cs ON cl.id_cliente_segmento = cs.id')
+    ->join('LEFT JOIN clientes_tipos AS ct ON cl.id_cliente_tipo = ct.id')
+    ->where('cl.id_empresa', $idEmpresa)
+    ->get();
+  }
+
+  public function verificaSeEmailExiste($email)
+  {
+    if ( ! $email) {
+      return false;
     }
 
-    public function verificaSeEmailExiste($email)
-    {
-        if ( ! $email) {
-            return false;
-        }
-
-        $query = $this->query("SELECT * FROM clientes WHERE email = '{$email}'");
-        if (count($query) > 0) {
-            return true;
-        }
-
-        return false;
+    $query = $this->query("SELECT * FROM clientes WHERE email = '{$email}'");
+    if (count($query) > 0) {
+      return true;
     }
 
-    public function verificaSeCnpjExiste($cnpj)
-    {
-        if ( ! $cnpj) {
-            return false;
-        }
+    return false;
+  }
 
-        $query = $this->query("SELECT * FROM clientes WHERE cnpj = '{$cnpj}'");
-        if (count($query) > 0) {
-            return true;
-        }
-
-        return false;
+  public function verificaSeCnpjExiste($cnpj)
+  {
+    if ( ! $cnpj) {
+      return false;
     }
 
-    public function verificaSeCpfExiste($cpf)
-    {
-        if ( ! $cpf) {
-            return false;
-        }
-
-        $query = $this->query("SELECT * FROM clientes WHERE cpf = '{$cpf}'");
-        if (count($query) > 0) {
-            return true;
-        }
-
-        return false;
+    $query = $this->query("SELECT * FROM clientes WHERE cnpj = '{$cnpj}'");
+    if (count($query) > 0) {
+      return true;
     }
 
-    public function seDadoNaoPertenceAoClienteEditado($nomeDoCampo, $valor, $idCliente)
-    {
-        $dadosCliente = $this->findBy("{$nomeDoCampo}", $valor);
-        if ($dadosCliente && $idCliente != $dadosCliente->id) {
-            return true;
-        }
+    return false;
+  }
 
-        return false;
+  public function verificaSeCpfExiste($cpf)
+  {
+    if ( ! $cpf) {
+      return false;
     }
+
+    $query = $this->query("SELECT * FROM clientes WHERE cpf = '{$cpf}'");
+    if (count($query) > 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public function seDadoNaoPertenceAoClienteEditado($nomeDoCampo, $valor, $idCliente)
+  {
+    $dadosCliente = $this->findBy("{$nomeDoCampo}", $valor);
+    if ($dadosCliente && $idCliente != $dadosCliente->id) {
+      return true;
+    }
+
+    return false;
+  }
 }

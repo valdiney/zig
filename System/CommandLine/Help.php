01341db6cd3e358a75e34bf216d01be5b3287db3
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-12-27 17:34:59 -0300

    docs:  adiciona helper para comando create dump

--- System/CommandLine/Help.php
+++ System/CommandLine/Help.php
@@ -12,7 +12,8 @@ class Help
     echo "• help: exibe estas informações \n\n";
     echo "• create [command] \n";
     echo "  • controller [ControllerName]: cria um controller \n";
-    echo "  • migration  [migration_name]: cria uma migration \n\n";
+    echo "  • migration  [migration description]: cria uma migration \n";
+    echo "  • dump       [dump description]:      cria um dump \n\n";
     echo "• migrate: migra banco de dados \n";
     echo "• dump   : popula banco de dados \n";
     echo "• hash   : gera um novo hash de senha \n";

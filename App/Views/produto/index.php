94a097efa6180a6c061bdaf238451ed651120327
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-12-27 18:37:28 -0300

    feat: adiciona coluna deleted_at aos produtos e permite desativar

--- App/Views/produto/index.php
+++ App/Views/produto/index.php
@@ -1,104 +1,112 @@
 <!--Usando o Html Components-->
 <?php
-use System\HtmlComponents\Modal\Modal;
+
 use System\HtmlComponents\FlashMessage\FlashMessage;
+use System\HtmlComponents\Modal\Modal;
+
 ?>
 
 <style type="text/css">
-	.imagem-produto {
-		width:40px;
-	    height:40px;
-	    object-fit:cover;
-	    object-position:center;
-	    border-radius:50%;
-	    border:1px solid silver;
-	}
+    .imagem-produto {
+        width: 40px;
+        height: 40px;
+        object-fit: cover;
+        object-position: center;
+        border-radius: 50%;
+        border: 1px solid silver;
+    }
 </style>
 
 <div class="row">
 
-	<div class="card col-lg-12 content-div">
-		<div class="card-body">
-	        <h5 class="card-title"><i class="fas fa-box-open"></i> Produtos</h5>
-	    </div>
-	    <!-- Mostra as mensagens de erro-->
-	    <?php FlashMessage::show();?>
+    <div class="card col-lg-12 content-div">
+        <div class="card-body">
+            <h5 class="card-title"><i class="fas fa-box-open"></i> Produtos</h5>
+        </div>
+        <!-- Mostra as mensagens de erro-->
+        <?php FlashMessage::show(); ?>
 
-	    <table class="table tabela-ajustada table-striped" style="width:100%">
-	        <thead>
-	            <tr>
-	            	<th>#</th>
-	                <th>Nome</th>
-	                <th>R$ Preço</th>
-	                <th style="text-align:right;padding-right:0">
-	                	<?php $rota = BASEURL.'/produto/modalFormulario';?>
-	                	<button onclick="modalFormularioProdutos('<?php echo $rota;?>', false);"
-	                		class="btn btn-sm btn-success" title="Novo Produto!">
-	                	    <i class="fas fa-plus"></i>
-	                    </button>
-	                </th>
-	            </tr>
-	        </thead>
-	        <tbody>
+        <table class="table tabela-ajustada table-striped" style="width:100%">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Ativo</th>
+                <th>R$ Preço</th>
+                <th style="text-align:right;padding-right:0">
+                    <?php $rota = BASEURL . '/produto/modalFormulario'; ?>
+                    <button onclick="modalFormularioProdutos('<?php echo $rota; ?>', false);"
+                            class="btn btn-sm btn-success" title="Novo Produto!">
+                        <i class="fas fa-plus"></i>
+                    </button>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
 
-	        	<?php foreach ($produtos as $produto):?>
-		            <tr>
-                        <td>
-                        	<?php if ( ! is_null($produto->imagem) && $produto->imagem != ''):?>
-		            			<center>
-		            				<img src="<?php echo BASEURL.'/'.$produto->imagem;?>" width="40" class="imagem-produto">
-		            			</center>
-		            		<?php else:?>
-		            			<center><i class="fab fa-product-hunt" style="font-size:40px"></i></center>
-		            		<?php endif;?>
-                        </td>
-		            	<td><?php echo $produto->nome;?></td>
-		            	<td><?php echo real($produto->preco);?></td>
+            <?php foreach ($produtos as $produto): ?>
+                <tr>
+                    <td>
+                        <?php if (!is_null($produto->imagem) && $produto->imagem != ''): ?>
+                            <center>
+                                <img src="<?php echo BASEURL . '/' . $produto->imagem; ?>" width="40"
+                                     class="imagem-produto">
+                            </center>
+                        <?php else: ?>
+                            <center><i class="fab fa-product-hunt" style="font-size:40px"></i></center>
+                        <?php endif; ?>
+                    </td>
+                    <td><?php echo $produto->nome; ?></td>
+                    <td>
+                        <?php echo $produto->deleted_at? "Não": "Sim"; ?>
+                    </td>
+                    <td><?php echo real($produto->preco); ?></td>
 
-		                <td style="text-align:right">
+                    <td style="text-align:right">
 
 
-						<div class="btn-group" role="group">
-						    <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						      <i class="fas fa-cogs"></i>
-						    </button>
-						    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                        <div class="btn-group" role="group">
+                            <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-secondary dropdown-toggle"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-cogs"></i>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
 
-						      <button class="dropdown-item" href="#"
-						      onclick="modalFormularioProdutos('<?php echo $rota;?>', '<?php echo $produto->id;?>')">
-						      	<i class="fas fa-edit"></i> Editar
-						      </button>
+                                <button class="dropdown-item" href="#"
+                                        onclick="modalFormularioProdutos('<?php echo $rota; ?>', '<?php echo $produto->id; ?>')">
+                                    <i class="fas fa-edit"></i> Editar
+                                </button>
 
-						      <!--<a class="dropdown-item" href="#">
-						      	<i class="fas fa-trash-alt" style="color:#cc6666"></i> Excluir
-						      </a>-->
+                                <!--<a class="dropdown-item" href="#">
+                                    <i class="fas fa-trash-alt" style="color:#cc6666"></i> Excluir
+                                </a>-->
 
-						    </div>
-						  </div>
-		                </td>
-		            </tr>
-		        <?php endforeach;?>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
 
-	        <tfoot></tfoot>
-	    </table>
+            <tfoot></tfoot>
+        </table>
 
-    <br>
+        <br>
 
-   </div>
+    </div>
 </div>
 
 <?php Modal::start([
     'id' => 'modalFormulario',
     'width' => 'modal-lg',
     'title' => 'Cadastrar Produtos'
-]);?>
+]); ?>
 
 <div id="formulario"></div>
 
-<?php Modal::stop();?>
+<?php Modal::stop(); ?>
 
 <script>
-	function modalFormularioProdutos(rota, id) {
+    function modalFormularioProdutos(rota, id) {
         var url = "";
 
         if (id) {
@@ -114,15 +122,15 @@ use System\HtmlComponents\FlashMessage\FlashMessage;
     }
 
     function salvarProduto() {
-    	if ($('#nome').val() == '') {
-			modalValidacao('Validação', 'Campo (Nome) deve ser preenchido!');
-			return false;
+        if ($('#nome').val() == '') {
+            modalValidacao('Validação', 'Campo (Nome) deve ser preenchido!');
+            return false;
 
-		} else if ($('#preco').val() == '') {
-			modalValidacao('Validação', 'Campo (Preço) deve ser preenchido!');
-			return false;
-		}
+        } else if ($('#preco').val() == '') {
+            modalValidacao('Validação', 'Campo (Preço) deve ser preenchido!');
+            return false;
+        }
 
-	    return true;
+        return true;
     }
 </script>
---
+++ Database/migrations/1609103876_adiciona_coluna_deleted_at_a_tabela_produtos.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `produtos` ADD `deleted_at` timestamp NULL DEFAULT NULL AFTER `updated_at`;

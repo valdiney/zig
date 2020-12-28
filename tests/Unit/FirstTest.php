5b6aba3eaa51a25842c2d0e55633f509bebd4e63
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-12-27 21:29:26 -0300

    feat: adiciona suporte a phpunit

---
+++ tests/Unit/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testInicial()
+    {
+        self::assertTrue(true);
+    }
+}

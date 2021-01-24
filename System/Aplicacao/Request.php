fbe93189cf6769667339df823f89d3e935e7e1a1
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-01-24 20:39:14 -0300

    feat: cria classe Request para receber entradas do usuário

---
+++ System/Aplicacao/Request.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace System\Aplicacao;
+
+class Request
+{
+    /** @var array */
+    private $get;
+    /** @var array */
+    private $post;
+    /** @var array */
+    private $session;
+
+    public function __construct(array $get, array $post = [], array $session = [])
+    {
+        $this->get = sanitizeMany($get);
+        $this->post = sanitizeMany($post);
+        $this->session = sanitizeMany($session);
+    }
+
+    public function get(string $name)
+    {
+        return $this->get[$name] ?? null;
+    }
+
+    public function post(string $name)
+    {
+        return $this->post[$name] ?? null;
+    }
+
+    public function session(string $name)
+    {
+        return $this->session[$name] ?? null;
+    }
+}

e63493fd0cb8e156cee65024bc435887254960da
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-01-24 20:54:21 -0300

    feat:  cria repositorio de permanencia de usuario

---
+++ System/Aplicacao/Permanencia/UsuarioPermanenciaRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace System\Aplicacao\Permanencia;
+
+use System\Aplicacao\Permanencia;
+use System\Entidade\EntidadeUsuario\EntidadeUsuario;
+
+interface UsuarioPermanenciaRepository
+{
+    /**
+     * @param EntidadeUsuario $usuario
+     * @return void
+     */
+    public function adicionaPermanencia(EntidadeUsuario $usuario): void;
+
+    /**
+     * @param EntidadeUsuario $usuario
+     * @return mixed
+     */
+    public function checaEstaConectado(EntidadeUsuario $usuario): bool;
+
+    /**
+     * @param EntidadeUsuario $usuario
+     * @return mixed
+     */
+    public function removePermanencia(EntidadeUsuario $usuario): void;
+}

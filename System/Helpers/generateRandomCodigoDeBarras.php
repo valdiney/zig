7fcdfcab651b3de8fe4db4f0befdaa1401582cac
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-16 11:20:17 -0300

    feat: fornece funcao para gerar codigo de barras aleatorio

---
+++ System/Helpers/generateRandomCodigoDeBarras.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!function_exists('generateRandomCodigoDeBarras')){
+    function generateRandomCodigoDeBarras($prefix = null, $posfix = null): string
+    {
+        return $prefix . random_int(111111111, 999999999) . $posfix;
+    }
+}

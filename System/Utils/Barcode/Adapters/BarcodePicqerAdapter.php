d48dd5e92c661eadd63726567656a35fb42ed9d0
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-16 10:47:34 -0300

    feat: ensure classe pode gerar um barcode em svg

--- System/Utils/Barcode/Adapters/BarcodePicqerAdapter.php
+++ System/Utils/Barcode/Adapters/BarcodePicqerAdapter.php
@@ -4,17 +4,22 @@ namespace System\Utils\Barcode\Adapters;
 
 use System\Utils\Barcode\Contracts\BarcodeAdapter;
 use Picqer\Barcode\BarcodeGeneratorSVG;
+use System\Utils\Barcode\Contracts\HasSvg;
+use System\Utils\Barcode\Exceptions\BarcodeSemCodigoException;
 
-class BarcodePicqerAdapter extends BarcodeAdapter
+class BarcodePicqerAdapter extends BarcodeAdapter implements HasSvg
 {
-    public function __construct()
-    {
-        $this->generator = new BarcodeGeneratorSVG();
-    }
-
     public function prepare(string $code): void
     {
-        $code = $this->generator->getBarcode($code, BarcodeGeneratorSVG::TYPE_KIX);
         $this->code = $code;
+    }
+
+    public function toSvg(): string
+    {
+        if (empty($this->code)) {
+            throw new BarcodeSemCodigoException();
+        }
+        $generator = new BarcodeGeneratorSVG();
+        return $generator->getBarcode($this->code, BarcodeGeneratorSVG::TYPE_KIX);
     }
 }

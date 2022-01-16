48fc2cbe53e63e331a1e0199a1a8363213b7cf09
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-16 10:37:08 -0300

    feat: garante que um svg será retornado na funcao toSvg

--- System/Utils/Barcode/Adapters/BarcodeSimpleAdapter.php
+++ System/Utils/Barcode/Adapters/BarcodeSimpleAdapter.php
@@ -17,6 +17,9 @@ class BarcodeSimpleAdapter extends BarcodeAdapter implements HasSvg
 
     public function toSvg(): string
     {
-        return '';
+        return '<svg height="100" width="100">
+                    <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+                    Sorry, your browser does not support inline SVG.
+                </svg>';
     }
 }
